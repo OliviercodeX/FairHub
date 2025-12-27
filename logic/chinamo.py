bfddac4c22737d7abb5c4dada33367dfcdf09777
Mainor-Martinez <mainorm660@gmail.com>
2025-12-27 15:42:05 -0600

    remove_product function done

--- logic/chinamo.py
+++ logic/chinamo.py
@@ -7,13 +7,11 @@ class Chinamo:
     def __init__(self,seller_name):
         self.seller_name = seller_name
         
-
-    
     def add_product(self,product, price):
         
         seller_code = None
         for code_key, seller_data in self.products.items():
-            print(seller_data['seller'])
+
             if seller_data['seller'] == self.seller_name:
                 seller_code = code_key
         
@@ -24,8 +22,6 @@ class Chinamo:
                 'price_product': price
             })
         
-        
-
 
         else:
             code = f'ATB{len(code_list)+1}'
@@ -38,15 +34,27 @@ class Chinamo:
                 }
             code_list.append('code')
 
-        
-        
-        
-        print(self.products)
 
-    def remove_product(self):
-        global code  #TODO To finish this method
-        for key, value in self.products(self.products.keys()):
-            pass
+    def remove_product(self,product): 
+
+         for code_key, seller_data in self.products.items(): #iterate over the dict self.products
+
+             if seller_data['seller'] == self.seller_name: #if we find the name seller we put a key to access after
+                 seller_code = code_key
+
+        
+         data_products = self.products[seller_code]['products'] #get the list products with the prices
+         finded = False #the flag for validate if we delete the product or doesn't exist
+         for elemt in data_products:
+            if elemt['name'] == product:
+                 finded = True
+                 data_products.remove(elemt) #remove the element
+                 print('Eliminación correcta')
+                 return finded
+         if not finded:
+             return finded
+         
+
 
 
     def get_total_stock(self):
@@ -54,5 +62,11 @@ class Chinamo:
 
 test1 = Chinamo('Marcos')
 test1.add_product('Arroz con leche', 500)
-test2 = Chinamo('Marcos')
-test2.add_product('Arroz con leche', 500)
+test1.add_product('arroz con pollo', 1500)
+test1.remove_product('arroz con pollo')
+test1.remove_product('arroz con pollo')
+
+
+productos = Chinamo.products
+for code, product in productos.items():
+    print(f'{product}\n')
