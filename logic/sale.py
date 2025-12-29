8a36e17aeb58ec76c450167a2ac4a136043c5c48
Mainor-Martinez <mainorm660@gmail.com>
2025-12-29 09:41:56 -0600

    Sales file completed; history structure finalized.

--- logic/sale.py
+++ logic/sale.py
@@ -1,11 +1,34 @@
 from time import strftime
 
-class Chinamo():
-    def __init__(self,chinamo_id, amount):
+class Sale():
+    def __init__(self,chinamo_id, items):
         self.chinamo_id = chinamo_id
-        self.amount = amount
+        self.items = items
+        self.total = self.calculate_total()
         self.timestamp = strftime("%a, %d %b %Y %H:%M:%S")
 
+    def calculate_total(self):
+        total = 0
+        for item in self.items:
+            total += item['qty'] * item['unit_price']
+        return total
+
     def to_dict(self):
-        pass
+        return {
+            'chinamo_id': self.chinamo_id,
+            'items': self.items,
+            'total': self.total,
+            'timestamp': self.timestamp
+        }
+
+
+#pruebas
+items = [
+    {"name": "Empanada", "qty": 3, "unit_price": 500},
+    {"name": "Refresco", "qty": 1, "unit_price": 800}
+]
+
+sale = Sale("ATB1", items)
+
+sale_dict = sale.to_dict()
 
