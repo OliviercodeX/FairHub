90dde16291372b60946a2c993a7519fa46a88922
Mainor-Martinez <mainorm660@gmail.com>
2025-12-30 12:42:28 -0600

    creation of methods with their respective functionalities

--- logic/fair_manager.py
+++ logic/fair_manager.py
@@ -1,20 +1,24 @@
 from chinamo import *
-
+from sale import *
 class Fair_manager():
     def __init__(self):
         self.chinamos = {}
         self.sales = []
 
     def create_chinamo(self, seller_name):
-        pass
+        get_chinamo = Chinamo(seller_name)
 
-    def add_product(self, chinamo_id, product):
-        pass
+    def add_product_main(self, product, price):
+        self.chinamos = Chinamo.add_product(product,price)
+        return self.chinamos
 
-    def register_sale(self, chinamo_id, product, quantity):
-        pass
+    def register_sale(self, chinamo_id, items):
+
+        data_sale = Sale.to_dict(chinamo_id, items)
+        
 
     def get_chinamo_income(self, chinamo_id):
+        total_income = 0
         pass
 
     def save_data():
